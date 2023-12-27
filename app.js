3f02eed93e0d91cc29e401fb118322b3443bb59f
Tom Gagliano <tgaglianoll@gmail.com>
2023-12-27 18:49:54 -0500

    add express, app, port, use, set and listen to app.js file

--- app.js
+++ app.js
@@ -1,34 +1,47 @@
-function Person(firstname, lastname) {
-  this.firstname = firstname;
-  this.lastname = lastname;
-}
+var express = require("express");
+var app = express();
 
-Person.prototype.greet = function () {
-  console.log("Hello, " + this.firstname + " " + this.lastname);
-};
+var port = process.env.PORT || 3000;
 
-var john = new Person("John", "Doe");
-console.log(john.firstname);
-john.greet();
+app.use("/assets", express.static(__dirname + "/public"));
 
-//the above is called prototypal inheritance
-// Constructor function for creating Person objects
-function Person(firstName, lastName, age) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.age = age;
+app.set("view engine", "ejs");
 
-  // Method to display the full name of the person
-  this.fullName = function () {
-    return this.firstName + " " + this.lastName;
-  };
-}
+app.listen(port);
 
-// Creating a new Person object using the constructor
-const person1 = new Person("John", "Doe", 30);
+// Don't pay attention to the below code.  It is from the beginning of the udemy course and has nothing to do with the above app.
 
-// Accessing properties and calling a method
-console.log(person1.firstName); // Output: John
-console.log(person1.lastName); // Output: Doe
-console.log(person1.age); // Output: 30
-console.log(person1.fullName()); // Output: John Doe
+// function Person(firstname, lastname) {
+//   this.firstname = firstname;
+//   this.lastname = lastname;
+// }
+
+// Person.prototype.greet = function () {
+//   console.log("Hello, " + this.firstname + " " + this.lastname);
+// };
+
+// var john = new Person("John", "Doe");
+// console.log(john.firstname);
+// john.greet();
+
+// //the above is called prototypal inheritance
+// // Constructor function for creating Person objects
+// function Person(firstName, lastName, age) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.age = age;
+
+//   // Method to display the full name of the person
+//   this.fullName = function () {
+//     return this.firstName + " " + this.lastName;
+//   };
+// }
+
+// // Creating a new Person object using the constructor
+// const person1 = new Person("John", "Doe", 30);
+
+// // Accessing properties and calling a method
+// console.log(person1.firstName); // Output: John
+// console.log(person1.lastName); // Output: Doe
+// console.log(person1.age); // Output: 30
+// console.log(person1.fullName()); // Output: John Doe
