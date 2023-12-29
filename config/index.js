32aac86fd260b5b183c4ac7de92477ac1e564a59
Tom Gagliano <tgaglianoll@gmail.com>
2023-12-28 21:39:55 -0500

    fix config.json file code and app.js code

---
+++ config/index.js
@@ -0,0 +1,17 @@
+var configValues = require("./config");
+
+module.exports = {
+  getDbConnectionString: function () {
+    return (
+      "mongodb+srv://" +
+      configValues.uname +
+      ":" +
+      configValues.pwd +
+      "@cluster0.id9uqmf.mongodb.net/?retryWrites=true&w=majority"
+    );
+  },
+};
+
+{
+  /* mongodb+srv://test:<passwordformongo>@cluster0.id9uqmf.mongodb.net/?retryWrites=true&w=majority" */
+}
